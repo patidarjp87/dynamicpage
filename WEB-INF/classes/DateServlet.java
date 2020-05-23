33efcd415a39ef0bdd166552419a0428de0de090
jaydeep <jaydeeppatidar87@gmail.com>
2020-05-23 23:44:40 +0530

    now modified java file due to same reason above

--- WEB-INF/classes/DateServlet.java
+++ WEB-INF/classes/DateServlet.java
@@ -1,1 +1,17 @@
-write any code here
+import javax.servlet.*;
+import java.io.*;
+import java.util.*; 
+public class DateServlet extends GenericServlet 
+{
+    public void service(ServletRequest req,ServletResponse res)throws ServletException,IOException
+    {
+        PrintWriter pw =null;
+        Date date=null;
+        res.setContentType("text/html");
+        pw=res.getWriter();
+        date=new Date();
+        pw.println("<h1 stype='align:center'>Date and time:::"+date+"</h1>");
+        pw.close();
+    }
+
+}
